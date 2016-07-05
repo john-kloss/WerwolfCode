e9b3d20a2193035ceaa81e2129670a2cbd6601c3
john-kloss <jonathan.kloss1@gmail.com>
2016-07-05 14:57:52 +0200

    JoinGameActivity updated

--- app/src/main/java/sopro/werwolf/JoinGameActivity.java
+++ app/src/main/java/sopro/werwolf/JoinGameActivity.java
@@ -1,14 +1,32 @@
 package sopro.werwolf;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JoinGameActivity extends AppCompatActivity {
+
+    JSONParser jsonParser = new JSONParser();
+    ProgressDialog pDiaglog;
+    String name;
+    private static final String url_create_player = "http://www-e.uni-magdeburg.de/jkloss/create_player.php";
+    private static final String TAG_SUCCESS = "success";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +49,59 @@ public class JoinGameActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.gameID);
         String gameID = editText.getText().toString();
 
+        new createPlayer().execute();
+
         // TODO: JSON - create_player.php 
+    }
+
+    /*
+     * This class inserts a name into 'player'.
+     * @param gameID
+     */
+    class createPlayer extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDiaglog = new ProgressDialog(JoinGameActivity.this);
+            pDiaglog.setMessage("Erstelle Spieler...");
+            pDiaglog.setIndeterminate(false);
+            pDiaglog.setCancelable(true);
+            pDiaglog.show();
+        }
+        protected String doInBackground(String... args) {
+            String name = "John";
+            String gameID = "0";
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("name", name));
+            params.add(new BasicNameValuePair("gameID", gameID));
+
+            JSONObject json = jsonParser.makeHttpRequest(url_create_player, "POST", params);
+
+            Log.d("Create Response", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt("success");
+
+                if (success == 1) {
+                    // successfully created product
+                    Snackbar.make(findViewById(R.id.joinGameView), "Spieler erstellt", Snackbar.LENGTH_SHORT).show();
+                    // closing this screen
+                    finish();
+                } else {
+                    // failed to create player
+                    Snackbar.make(findViewById(R.id.joinGameView), "Spieler nicht erstellt", Snackbar.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        public void onPostExecute(String file_url) {
+            pDiaglog.dismiss();
+        }
     }
 
 }
